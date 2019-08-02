fdc6b639858047799d8d11ef9a6c69b7ee562066
Emily Frost <emfrost@gmail.com>
2019-08-02 13:04:25 -0600

    working on logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -53,8 +53,13 @@ module.exports = function(app) {
 
     app.post("/api/voters", function (req, res) {
         var userScore = req.body.scores;
-        var scoresArr = [];
-        var bestMatch = 0;
+        var scoresArr = [20, 30, 40, 50, 60, 70];
+        var bestMatch1 = 0;
+        var bestMatch2 = 0;
+        var bestMatch3 = 0;
+        var bestMatch4 = 0;
+        var bestMatch5 = 0;
+
 
     for (var i = 0; i < candidates.length; i++) {
         var scoreDiff = 0;
@@ -66,7 +71,11 @@ module.exports = function(app) {
     
     for (var i = 0; i < scoresArr.length; i++) {
         if (scoresArr[i] <= scoresArr[bestMatch]) {
-            bestMatch = i;
+            bestMatch1 = i;
+            console.log("bestmatch1: " + bestMatch1)
+        } else if ((scoresArr[i] <= scoresArr[bestMatch2]) && (bestMatch2 > bestMatch1)) {
+            bestMatch2 = i; 
+            console.log("bestmatch2: " + bestMatch2)
         }
     }
 
@@ -74,7 +83,7 @@ module.exports = function(app) {
     var bestCandidate = candidates[bestMatch];
     res.json(bestCandidate);
         voters.push(req.body);
-        // res.status(200).send(userScore);
+        res.status(200).send(userScore);
     });
 
 
