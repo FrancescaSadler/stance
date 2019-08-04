0f6484370dd0fee2b9502a1575f6836da6522900
Emily Frost <emfrost@gmail.com>
2019-08-03 19:40:22 -0600

    commented out friendfinder code and added app.put example in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,70 +21,90 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  // --note that PUT route was added by EGF from a todos list example
+  // PUT route for updating examples. We can get the updated example from req.body 
+  app.put("/api/examples", function(req, res) {
+  // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Example.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
 };
+
+
 
 
 //CODE BELOW FROM FRIENDFINDER -- NEEDS UPDATING TO INCORPORATE 5 CANDIDATES
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-var path = require("path");
-var voters = require("../data/voters");
-var candidates = require("../data/candidates");
+// var path = require("path");
+// var voters = require("../data/voters");
+// var candidates = require("../data/candidates");
 
 
-module.exports = function(app) {
+// module.exports = function(app) {
 
 
-    app.get("/api/candidates", function(req, res){
-        return res.json(candidates);
-    });
+//     app.get("/api/candidates", function(req, res){
+//         return res.json(candidates);
+//     });
 
-    app.get("/api/candidates/:routeName", function(req, res) {
-        var chosen = req.params.routeName;
+//     app.get("/api/candidates/:routeName", function(req, res) {
+//         var chosen = req.params.routeName;
 
-        for (var i = 0; i < candidates.length; i++) {
-            if (chosen == candidates[i].routeName) {
-                return res.json(candidates[i]);
-            }
-        }
+//         for (var i = 0; i < candidates.length; i++) {
+//             if (chosen == candidates[i].routeName) {
+//                 return res.json(candidates[i]);
+//             }
+//         }
 
-        return res.json(false);
-    });
+//         return res.json(false);
+//     });
 
-    app.post("/api/voters", function (req, res) {
-        var userScore = req.body.scores;
-        var scoresArr = [20, 30, 40, 50, 60, 70];
-        var bestMatch1 = 0;
-        var bestMatch2 = 0;
-        var bestMatch3 = 0;
-        var bestMatch4 = 0;
-        var bestMatch5 = 0;
+//     app.post("/api/voters", function (req, res) {
+//         var userScore = req.body.scores;
+//         var scoresArr = [20, 30, 40, 50, 60, 70];
+//         var bestMatch1 = 0;
+//         var bestMatch2 = 0;
+//         var bestMatch3 = 0;
+//         var bestMatch4 = 0;
+//         var bestMatch5 = 0;
 
 
-    for (var i = 0; i < candidates.length; i++) {
-        var scoreDiff = 0;
-        for (var j = 0; j < userScore.length; j++) {
-            scoreDiff += (Math.abs(parseInt(candidates[i].scores[j]) - parseInt(userScore[j])))
-        }
-        scoresArr.push(scoreDiff);
-    } 
+//     for (var i = 0; i < candidates.length; i++) {
+//         var scoreDiff = 0;
+//         for (var j = 0; j < userScore.length; j++) {
+//             scoreDiff += (Math.abs(parseInt(candidates[i].scores[j]) - parseInt(userScore[j])))
+//         }
+//         scoresArr.push(scoreDiff);
+//     } 
     
-    for (var i = 0; i < scoresArr.length; i++) {
-        if (scoresArr[i] <= scoresArr[bestMatch]) {
-            bestMatch1 = i;
-            console.log("bestmatch1: " + bestMatch1)
-        } else if ((scoresArr[i] <= scoresArr[bestMatch2]) && (bestMatch2 > bestMatch1)) {
-            bestMatch2 = i; 
-            console.log("bestmatch2: " + bestMatch2)
-        } 
-    }
+//     for (var i = 0; i < scoresArr.length; i++) {
+//         if (scoresArr[i] <= scoresArr[bestMatch]) {
+//             bestMatch1 = i;
+//             console.log("bestmatch1: " + bestMatch1)
+//         } else if ((scoresArr[i] <= scoresArr[bestMatch2]) && (bestMatch2 > bestMatch1)) {
+//             bestMatch2 = i; 
+//             console.log("bestmatch2: " + bestMatch2)
+//         } 
+//     }
 
     
-    var bestCandidate = candidates[bestMatch];
-    res.json(bestCandidate);
-        voters.push(req.body);
-        res.status(200).send(userScore);
-    });
+//     var bestCandidate = candidates[bestMatch];
+//     res.json(bestCandidate);
+//         voters.push(req.body);
+//         res.status(200).send(userScore);
+//     });
 
 
-};
+// };
