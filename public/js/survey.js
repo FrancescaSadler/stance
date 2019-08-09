4174ece2c3dff27cb1ba3fb4d4bf0e7fb1911f3e
unknown <fsadler9@gmail.com>
2019-08-08 20:28:11 -0600

    id and playing around

--- public/js/survey.js
+++ public/js/survey.js
@@ -93,3 +93,42 @@
       alert("Please fill out all fields before submitting!");
     }
   });
+
+  //playing around 
+
+  function createCandidate() {
+
+    var clearBtn = $("<button>").attr("id", "clear").addClass("btn btn-primary bellhop center-block").text("Clear")
+    $("#resultsWrap").append(clearBtn);
+  
+      candidateFilter.forEach(function (result, i) {
+          var candidateName = result.candidate.name;
+          // var candidateImage = result.restaurant.thumb;
+  
+          var candidateWrap = $("<div>").addClass("card mb-3 center").attr("id", "candidateName-" + i).attr("data-index", i);
+  
+          var row = $("<div>").addClass("row no-gutters");
+  
+          // var col1 = $("<div>").addClass("col-md-4").attr("id", "candidtaeImg-" + i);
+          // var img = $("<img>").attr("src", candidateImage).attr("alt", "Your candidate");
+          // $(col1).append(img);
+  
+          var col2 = $("<div>").addClass("col-md-4 text-center")
+          var content = $("<div>").addClass("card-body");
+  
+          var h2 = $("<h2>").addClass("card-title").attr("id", "candidateName-"+i).text(candidateName);
+          var btn = $("<button>").attr("type", "button").addClass("btn btn-primary bellhop buttonMore").attr("data-index", i).attr("id", "ButtonMore-"+i).attr("data-price", pricePoint).text("Tell Me More")
+  
+          $(content).append(h2, btn)
+          $(col2).append(content);
+  
+          $(row).append(col1, col2);
+  
+          $(candidateWrap).append(row)
+  
+  
+          $("#resultsWrap").append(candidateWrap);
+          $("#resultsWrap").slideDown('slow');
+      });
+  
+  }
