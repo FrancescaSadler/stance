3ebf49a660ce76a00cdb80a1df4713990be0e05d
Savannah Israel <sisrael0405@gmail.com>
2019-08-10 13:48:08 -0600

    ALL OF IT AND SCURVY

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -47,10 +47,20 @@ module.exports = function (app) {
   app.post("/api/voters", function(req, res) {
     // console.log("API voters route was hit");
     // console.log(req.body);
+    var firstName = req.body.name;
     var userScore = req.body['scores[]'];
     var scoresArr = [];
-  // db.Candidates.findAll().then(function(candidates))
-    console.log(req.body);
+    console.log("userScore", userScore);
+    console.log("userScore[0]", typeof userScore[0]);
+
+  db.Candidate.findAll({}).then(function(candidates) {
+    console.log("candidates", candidates);
+    console.log("candidates[0]", candidates[0]);
+
+    var candidateScores = candidates[0].scores.split(",")
+    console.log("candidates[0]", typeof candidateScores[0]);
+    
+
     for (var i = 0; i < candidates.length; i++) {
       var scoreDiff = 0;
       var candidateScores = candidates[i].scores.split(",");
@@ -58,15 +68,41 @@ module.exports = function (app) {
         for (var j = 0; j < candidateScores.length; j++) {
             scoreDiff += (Math.abs(parseInt(candidateScores[j]) - parseInt(userScore[j])))
         // console.log(scoreDiff);
-        
       }
+      
       scoresArr.push(scoreDiff);
-       
-        // console.log("scores array index", scoresArr[i]);
-        // console.log("candidates index", candidates[i]);
-    } 
-    console.log(`user score         ${userScore}`);
-    console.log(`scores array ${scoresArr}`);
+      console.log("scores array" , scoresArr);
+      console.log(typeof scoresArr[1]);
+  };
+    console.log(req.body);
+    
+    var newVoter = {
+      firstName: firstName,
+      amyKlobuchar: `${100 - scoresArr[0]}`,
+      andrewYang: `${scoresArr[1]}`,
+      bernieSanders: `${scoresArr[2]}`,
+      betoOrourke: `${scoresArr[3]}`,
+      coryBooker: `${scoresArr[4]}`,
+      donaldTrump: `${scoresArr[5]}`,
+      elizabethWarren: `${scoresArr[6]}`,
+      jayInslee: `${scoresArr[7]}`,
+      joeBiden: `${scoresArr[8]}`,
+      johnDelaney: `${scoresArr[9]}`, 
+      johnHickenloop: `${scoresArr[10]}`,
+      julianCastro: `${scoresArr[11]}`,
+      kamalaHarris: `${scoresArr[12]}`,
+      kristenGillibrand: `${scoresArr[13]}`,
+      marianneWilliamson: `${scoresArr[14]}`,
+      peteButtigieg: `${scoresArr[15]}`,
+      tulsiGabbard: `${scoresArr[16]}`
+    };
+    console.log(`new Voter created WOO: ${newVoter}`)
+    
+    db.Voter.create(newVoter).then(function(newVoter) {
+      res.send(newVoter);
+    });
+    
+  });
     // db.Voter.create(req.body).then(function(dbExample) {
     //   res.json(dbExample);
     // });
